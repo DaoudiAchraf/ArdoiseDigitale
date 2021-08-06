69573780a0033ca89d273e8bea0f3b0becc6c9c7
wathekk <wathek9711@gmail.com>
2021-08-06 15:37:30 +0100

    fixed common

--- services/Common.js
+++ services/Common.js
@@ -1,6 +1,5 @@
 import client from "./Client";
 
-const getOrders = (ardoiseId) => client.get(`/api/orders/${ardoiseId}`);
 const getOrdersByState = (state) => client.post(`/api/orders/state`, state);
 const getArdoises = () => client.get('/api/ardoise');
 const getOrdersByArdoise = (ardoiseId) => client.get(`/api/orders/${ardoiseId}`);
